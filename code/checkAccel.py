2fa33b778980f876c7c99d2102f1331b123250e0
smsolivier <smsolivier@gmail.com>
2017-02-08 21:57:13 -0600

    update

--- code/checkAccel.py
+++ code/checkAccel.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 from sn import * 
 
+from hidespines import * 
+
 ''' compare the number of iterations for S_2 with DSA and without ''' 
 
 N = 100 # number of edges 
@@ -21,7 +23,7 @@ itmu = np.zeros(len(Sigmaa))
 itdsa = np.zeros(len(Sigmaa))
 tol = 1e-6
 
-n = 16
+n = 8
 
 for i in range(len(Sigmaa)):
 
@@ -37,10 +39,13 @@ for i in range(len(Sigmaa)):
 	dsa = DSA(N, n, Sigmaa[i], Sigmat, q, xb=20)
 	xdsa, phidsa, itdsa[i] = dsa.sourceIteration(tol)
 
-plt.plot(c, it, '-o', label='S'+str(n)+' SI')
-plt.plot(c, itmu, '-o', label='S'+str(n)+' Accel')
-plt.plot(c, itdsa, '-o', label='S'+str(n)+' DSA')
-plt.legend(loc='best')
+plt.plot(c, it, '-o', label='S$_8$ No Accel')
+plt.plot(c, itmu, '-o', label='S$_8$ Edd. Accel')
+plt.plot(c, itdsa, '-o', label='S$_8$ DSA')
+hidespines(plt.gca())
+plt.yscale('log')
+plt.legend(loc='best', frameon=False)
 plt.xlabel(r'$\Sigma_s/\Sigma_t$')
 plt.ylabel('Number of Iterations')
+plt.savefig('accel.pdf')
 plt.show()

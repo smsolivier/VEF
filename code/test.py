502ccdf211f9ed909a49dfc83ca6f4602760f8f0
smsolivier <smsolivier@gmail.com>
2017-02-17 17:32:52 -0600

    2/17

--- code/test.py
+++ code/test.py
@@ -3,17 +3,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 25
+N = 100
 
 xb = 1 
-x = np.linspace(0, xb, N)
-
 dx = xb/N
+x = np.linspace(dx/2, xb - dx/2, N)
 
 psiR = np.zeros((2,N))
 psiL = np.zeros((2,N)) 
 
-sigmat = .1
+sigmat = 1
 
 # sweep left to right (mu > 0)
 psi0R = 0 # vacuum 
@@ -30,7 +29,7 @@ for i in range(1, N):
 	A[1,1] = -1/2 + sigmat*dx/2 + 1
 
 	# rhs 
-	b = np.ones(2)
+	b = np.ones(2)*dx
 	b[0] += psiR[0,i-1]
 
 	ans = np.linalg.solve(A, b)
@@ -38,18 +37,18 @@ for i in range(1, N):
 	psiL[0,i] = ans[0] 
 	psiR[0,i] = ans[1] 
 
-psiL[1,-1] = psiR[0,-1] 
+# psiL[1,-1] = psiR[0,-1] 
 for i in range(N-2, -1, -1):
 
 	# left 
-	A[0,0] = 1/2 + sigmat*dx/2 + 1
+	A[0,0] = -1/2 + sigmat*dx/2 + 1
 	A[0,1] = -1/2
 
 	# right
 	A[1,0] = 1/2
 	A[1,1] = 1/2 + sigmat*dx/2 
 
-	b = np.ones(2)
+	b = np.ones(2)*dx
 	b[1] += psiL[1,i+1]
 
 	ans = np.linalg.solve(A, b)
@@ -57,7 +56,8 @@ for i in range(N-2, -1, -1):
 	psiL[1,i] = ans[0] 
 	psiR[1,i] = ans[1]
 
-psi = 1/2*(psiL[0,:] + psiR[0,:]) + 1/2*(psiL[1,:] + psiR[1,:])
-
+psi = .5*(psiL[0,:] + psiR[0,:]) + .5*(psiL[1,:] + psiR[1,:])
+plt.plot(x, psiL[0,:] + psiR[0,:])
+plt.plot(x, psiL[1,:] + psiR[1,:])
 plt.plot(x, psi)
 plt.show()

31d75a09874341c4e543094abd5384bca5278c36
smsolivier <smsolivier@gmail.com>
2017-02-18 12:40:56 -0600

    fucking windows drive

--- code/test.py
+++ code/test.py
@@ -3,7 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 100
+from mc import * 
+
+N = 50
 
 xb = 1 
 dx = xb/N
@@ -17,6 +19,50 @@ sigmat = 1
 # sweep left to right (mu > 0)
 psi0R = 0 # vacuum 
 A = np.zeros((2,2))
+
+# left 
+A[0,0] = -1/2 + sigmat*dx/2 + 1
+A[0,1] = -1/2
+
+# right
+A[1,0] = 1/2
+A[1,1] = 1/2 + sigmat*dx/2 
+
+b = np.ones(2)*dx
+
+ans = np.linalg.solve(A, b)
+
+psiL[1,-1] = ans[0] 
+psiR[1,-1] = ans[1]
+for i in range(N-2, -1, -1):
+
+	# left 
+	A[0,0] = -1/2 + sigmat*dx/2 + 1
+	A[0,1] = -1/2
+
+	# right
+	A[1,0] = 1/2
+	A[1,1] = 1/2 + sigmat*dx/2 
+
+	b = np.ones(2)*dx
+	b[1] += psiL[1,i+1]
+
+	ans = np.linalg.solve(A, b)
+
+	psiL[1,i] = ans[0] 
+	psiR[1,i] = ans[1]
+
+# bc
+A[0,0] = 1/2 + sigmat*dx/2 
+A[0,1] = 1/2 
+A[1,0] = -1/2 
+A[1,1] = -1/2 + sigmat*dx/2 + 1 
+
+b = np.ones(2)*dx 
+b[0] += psiL[1,0] 
+ans = np.linalg.solve(A, b)
+psiL[0,0] = ans[0]
+psiR[0,0] = ans[1] 
 
 for i in range(1, N):
 
@@ -37,27 +83,12 @@ for i in range(1, N):
 	psiL[0,i] = ans[0] 
 	psiR[0,i] = ans[1] 
 
-# psiL[1,-1] = psiR[0,-1] 
-for i in range(N-2, -1, -1):
-
-	# left 
-	A[0,0] = -1/2 + sigmat*dx/2 + 1
-	A[0,1] = -1/2
-
-	# right
-	A[1,0] = 1/2
-	A[1,1] = 1/2 + sigmat*dx/2 
-
-	b = np.ones(2)*dx
-	b[1] += psiL[1,i+1]
-
-	ans = np.linalg.solve(A, b)
-
-	psiL[1,i] = ans[0] 
-	psiR[1,i] = ans[1]
+xmc, flux, leakL, leakR = monteCarlo(1000, 10, )
 
 psi = .5*(psiL[0,:] + psiR[0,:]) + .5*(psiL[1,:] + psiR[1,:])
-plt.plot(x, psiL[0,:] + psiR[0,:])
-plt.plot(x, psiL[1,:] + psiR[1,:])
+# plt.plot(x, psiL[0,:] + psiR[0,:])
+# plt.plot(x, psiL[1,:] + psiR[1,:])
 plt.plot(x, psi)
+# plt.plot(psiL[0,:], '-o')
+# plt.plot(psiR[0,:], '-o')
 plt.show()

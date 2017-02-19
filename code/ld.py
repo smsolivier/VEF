a7638c862983d526d1b541ffc096e1df363baa84
smsolivier <smsolivier@gmail.com>
2017-02-18 18:31:43 -0600

    ld accel

--- code/ld.py
+++ code/ld.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 
 import sn as DD
 
+from mhfem_acc import * 
+
 class LD:
 	''' Linear Discontinuous Galerkin Sn ''' 
 
-	def __init__(self, xe, n, Sigmaa, Sigmat, q):
-		''' 
-			Inputs:
+	def __init__(self, xe, n, Sigmaa, Sigmat, q, ACCEL=True):
+		''' Inputs:
 				xe: cell edges 
 				n: number of discrete ordinates 
 				Sigmaa: absorption XS (function)
@@ -20,9 +21,12 @@ class LD:
 
 		self.N = np.shape(xe)[0] - 1 # number of cell centers 
 		self.n = n # number of discrete ordinates 
+		self.ACCEL = ACCEL
 
 		self.x = np.zeros(self.N) # cell centered locations 
 		self.h = np.zeros(self.N) # cell widths at cell center 
+
+		self.xe = xe # cell edge array 
 
 		for i in range(1, self.N+1):
 
@@ -43,23 +47,42 @@ class LD:
 		self.psiL = np.zeros((self.n, self.N)) # LD left point  
 		self.psiR = np.zeros((self.n, self.N)) # LD right point 
 
-		self.psi = np.zeros((self.n, self.N)) # cell edged flux 
+		self.psi = np.zeros((self.n, self.N+1)) # cell edged flux 
 
 		self.phi = np.zeros(self.N) # store flux 
 
 		# generate mu's, mu is arranged negative to positive 
 		self.mu, self.w = np.polynomial.legendre.leggauss(n)
 
-	def sweep(self, phiL, phiR):
+	def sweep(self, phi):
 
-		self.sweepRL(phiL, phiR)
-		self.sweepLR(phiL, phiR)
+		# sweep for psi 
+		self.sweepRL(phi)
+		self.sweepLR(phi)
 
-		phiL, phiR = self.integratePsi()
+		psi = self.edgePsi()
 
-		return phiL, phiR 
+		if (self.ACCEL == False):
 
-	def sweepLR(self, phiL, phiR):
+			# get flux 
+			phi = self.integratePsi(psi)
+
+			return phi 
+
+		elif (self.ACCEL == True):
+
+			# compute eddington factor 
+			mu2 = self.getEddington(psi)
+
+			# create MHFEM object 
+			sol = MHFEM(self.xe, mu2, self.Sigmaa, self.Sigmat, BCL=0, BCR=1)
+
+			# solve for phi 
+			x, phi = sol.solve(self.q)
+
+			return phi # return accelerated flux 
+
+	def sweepLR(self, phi):
 		''' sweep left to right (mu > 0) ''' 
 
 		A = np.zeros((2,2)) # store psiL, psiR coefficients 
@@ -82,7 +105,7 @@ class LD:
 				A[1,1] = -self.mu[i]/2 + self.Sigmat(self.x[j])*h/2 + self.mu[i] # psiR 
 
 				# rhs 
-				b[0] = self.Sigmas(self.x[j])*h/4*phiL[j] + self.q[j]*h/4 # left 
+				b[0] = self.Sigmas(self.x[j])*h/4*phi[j] + self.q[j]*h/4 # left 
 				if (j == 0): # boundary condition 
 
 					# reflecting 
@@ -91,7 +114,7 @@ class LD:
 				else: # normal sweep 
 					b[0] += self.mu[i]*self.psiR[i,j-1] # upwind term 
 
-				b[1] = self.Sigmas(self.x[j])*h/4*phiR[j] + self.q[j+1]*h/4 # right 
+				b[1] = self.Sigmas(self.x[j])*h/4*phi[j+1] + self.q[j+1]*h/4 # right 
 
 				ans = np.linalg.solve(A, b) # solve for psiL, psiR 
 
@@ -99,7 +122,7 @@ class LD:
 				self.psiL[i,j] = ans[0] 
 				self.psiR[i,j] = ans[1] 
 
-	def sweepRL(self, phiL, phiR):
+	def sweepRL(self, phi):
 		''' sweep right to left (mu < 0) ''' 
 
 		A = np.zeros((2,2)) # store psiL, psiR coefficients 
@@ -122,8 +145,8 @@ class LD:
 				A[0,1] = self.mu[i]/2 
 
 				# rhs 
-				b[0] = self.Sigmas(self.x[j])*h/4*phiL[j] + self.q[j]*h/4 # left 
-				b[1] = self.Sigmas(self.x[j])*h/4*phiR[j] + self.q[j+1]*h/4 # right 
+				b[0] = self.Sigmas(self.x[j])*h/4*phi[j] + self.q[j]*h/4 # left 
+				b[1] = self.Sigmas(self.x[j])*h/4*phi[j+1] + self.q[j+1]*h/4 # right 
 
 				if (j == self.N-1): # boundary condition 
 					b[1] += 0 # vacuum bc 
@@ -137,53 +160,52 @@ class LD:
 				self.psiL[i,j] = ans[0] 
 				self.psiR[i,j] = ans[1] 
 
-	def integratePsi(self):
+	def integratePsi(self, psi):
 		''' use guass legendre quadrature points to integrate psi ''' 
 
-		phi = np.zeros(self.N)
+		phi = np.zeros(self.N+1)
 
-		phiL = np.zeros(self.N) # store LD left flux 
-		phiR = np.zeros(self.N) # store LD right flux 
-
-		# loop through angles
 		for i in range(self.n):
 
-			phiL += self.psiL[i,:] * self.w[i] 
-			phiR += self.psiR[i,:] * self.w[i]
+			phi += psi[i,:] * self.w[i] 
 
-		return phiL, phiR  
+		return phi 
 
 	def edgePsi(self):
 		''' get celled edge angular flux accounting for up/down winding ''' 
+
+		psi = np.zeros((self.n, self.N+1)) # store cell edge psi 
 
 		for i in range(self.n):
 
 			if (self.mu[i] > 0): # positive angles 
 
 				# set boundary values 
-				self.psi[i,0] = self.psiL[self.mu == -self.mu[i],0] # reflecting 
+				psi[i,0] = self.psiL[self.mu == -self.mu[i],0] # reflecting 
 
 				for j in range(self.N):
 
-					self.psi[i,j+1] = self.psiR[i,j] # psi_j+1/2 = psi_i,R  
+					psi[i,j+1] = self.psiR[i,j] # psi_j+1/2 = psi_i,R  
 
 			else: # negative angles 
 
 				# set boundary values 
-				self.psi[i,-1] = 0 # vacuum 
+				psi[i,-1] = 0 # vacuum 
 
 				for j in range(self.N-1, -1, -1):
 
-					self.psi[i,j] = self.psiL[i,j] # psi_j-1/2 = psi_i,L
+					psi[i,j] = self.psiL[i,j] # psi_j-1/2 = psi_i,L
 
-	def getEddington(self):
+		return psi 
+
+	def getEddington(self, psi):
 		''' compute <mu^2> ''' 
 
 		phi = np.zeros(self.N + 1) # cell edge flux 
 
 		for i in range(self.n):
 
-			phi += self.psi[i,:] * self.w[i]
+			phi += psi[i,:] * self.w[i]
 
 		# Eddington factor 
 		mu2 = np.zeros(self.N+1) 
@@ -191,7 +213,7 @@ class LD:
 		top = 0 
 		for i in range(self.n):
 
-			top += self.mu[i]**2 * self.psi[i,:] * self.w[i] 
+			top += self.mu[i]**2 * psi[i,:] * self.w[i] 
 
 		mu2 = top/phi 
 
@@ -201,19 +223,14 @@ class LD:
 		''' lag RHS of transport equation and iterate until flux converges ''' 
 
 		it = 0 # store number of iterations 
-		phiL = np.zeros(self.N) # LD left flux 
-		phiR = np.zeros(self.N) # LD right flux 
-		phi = np.zeros(self.N) # cell centered flux 
-		edd = np.zeros(self.N) # store eddington factor 
+		phi = np.zeros(self.N+1) # cell centered flux 
 
 		while (True):
 
 			# store old flux 
 			phi_old = np.copy(phi) 
 
-			phiL, phiR = self.sweep(phiL, phiR) # update flux 
-
-			phi = .5*(phiL + phiR) # cell centered flux 
+			phi = self.sweep(phi_old) # update flux 
 
 			# check for convergence 
 			if (np.linalg.norm(phi - phi_old, 2)/np.linalg.norm(phi, 2) < tol):
@@ -226,26 +243,31 @@ class LD:
 		print('Number of iterations =', it) 
 
 		# return spatial locations, flux and number of iterations 
-		return self.x, phi, it 
+		return self.xe, phi, it 
 
-N = 5
+N = 50
 xb = 2
 x = np.linspace(0, xb, N)
 Sigmaa = lambda x: .1 
 Sigmat = lambda x: .83 
 q = np.ones(N) 
 
-n = 16
+n = 8
 
 ld = LD(x, n, Sigmaa, Sigmat, q)
 
 sn = DD.Sn(x, n, Sigmaa, Sigmat, q)
 
+mu = DD.muAccel(x, n, Sigmaa, Sigmat, q)
+
 x, phi, it = ld.sourceIteration(1e-6)
 
 xsn, phisn, itsn = sn.sourceIteration(1e-6)
 
-plt.plot(x, phi, label='LD')
+xmu, phimu, itmu = mu.sourceIteration(1e-6)
+
+plt.plot(x, phi, label='LD Edd')
 plt.plot(xsn, phisn, label='DD')
+plt.plot(xmu, phimu, label='DD Edd')
 plt.legend(loc='best')
 plt.show()

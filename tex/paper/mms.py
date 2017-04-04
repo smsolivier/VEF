6e017500ac4329cdbaabba43d6c99646d1e56d93
smsolivier <smsolivier@gmail.com>
2017-04-04 16:07:32 -0500

    make for paper

---
+++ tex/paper/mms.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys 
+sys.path.append('../../code')
+
+import ld as LD
+import dd as DD 
+
+from scipy.interpolate import interp1d 
+
+from hidespines import * 
+
+from R2 import * 
+
+''' Test order of accuracy for LD options ''' 
+
+if (len(sys.argv) > 1):
+	outfile = sys.argv[1] 
+else:
+	outfile = None 
+
+def getOrder(sol, N, tol, label):
+
+	print('Method =', sol[0].name)
+
+	phi_mms = lambda x: np.sin(np.pi*x/xb) # exact solution 
+
+	err = np.zeros(len(sol))
+	for i in range(len(sol)):
+
+		sol[i].setMMS()
+		# make video 
+		# x, phi, it = sol[i].sourceIteration(tol, PLOT='phi' + str(N[i]))
+		x, phi, it = sol[i].sourceIteration(tol, 1000)
+
+		phi_int = interp1d(x, phi)
+
+		# err[i] = np.fabs(phi_mms(xb/2) - phi_int(xb/2))/phi_mms(xb/2)
+		err[i] = np.linalg.norm(phi_mms(x) - phi, 2)/np.linalg.norm(phi_mms(x), 2)
+
+	# 	plt.plot(x, phi, '-o')
+
+	# plt.show()
+
+	fit = np.polyfit(np.log(1/N), np.log(err), 1)
+
+	# fit equation 
+	f = lambda x: np.exp(fit[1]) * x**(fit[0])
+
+	# R^2 value
+	r2 = rsquared(err, f(1/N))
+
+	print(fit[0], np.exp(fit[1]), r2)
+
+	plt.loglog(xb/N, err, '-o', clip_on=False, label=label)
+
+	return err
+
+# N = np.array([80, 160, 320, 640, 1280])
+N = np.logspace(1.2, 3, 5)
+N = np.array([int(x) for x in N])
+
+n = 8 
+
+Sigmaa = lambda x: .1
+Sigmat = lambda x: 1
+
+xb = 5
+
+tol = 1e-10 
+
+# make solver objects 
+ed = [LD.LD(np.linspace(0, xb, x+1), n, Sigmaa, 
+	Sigmat, np.ones((n,x))) for x in N]
+
+ed00 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
+	Sigmat, np.ones((n, x)), OPT=0, GAUSS=0) for x in N]
+
+ed01 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
+	Sigmat, np.ones((n, x)), OPT=0, GAUSS=1) for x in N]
+
+ed10 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
+	Sigmat, np.ones((n, x)), OPT=1, GAUSS=0) for x in N]
+
+ed11 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
+	Sigmat, np.ones((n, x)), OPT=1, GAUSS=1) for x in N]
+
+ed20 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
+	Sigmat, np.ones((n,x)), OPT=2, GAUSS=0) for x in N]
+
+ed21 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
+	Sigmat, np.ones((n, x)), OPT=2, GAUSS=1) for x in N]
+
+# get order of accuracy 
+# err = getOrder(ed, N, tol, 'LD')
+err00 = getOrder(ed00, N, tol, 'MHFEM Edges, No Gauss')
+err01 = getOrder(ed01, N, tol, 'Maintain Slopes, No Gauss')
+err10 = getOrder(ed10, N, tol, 'MHFEM Edges, Gauss')
+err11 = getOrder(ed11, N, tol, 'Maintain Slopes, Gauss')
+err20 = getOrder(ed20, N, tol, 'vanLeer, No Gauss')
+err21 = getOrder(ed21, N, tol, 'vanLeer, Gauss')
+
+# plt.loglog(xb/N, err20[-1]/(xb/N[-1])**2*(xb/N)**2, 
+	# color='k', alpha=.7, label='Slope = 2')
+plt.legend(loc='best', frameon=False)
+plt.xlabel(r'$h$', fontsize=20)
+plt.ylabel('Error', fontsize=20)
+hidespines(plt.gca())
+if (outfile != None):
+	plt.savefig(outfile, transparent=True)
+else:
+	plt.show()
---
+++ tex/paper/perm_dl.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys 
+
+sys.path.append('../../code')
+
+import ld as LD 
+
+from exactDiff import exactDiff
+
+''' compare the permuations of linear representation in diffusion limit ''' 
+
+if (len(sys.argv) > 1):
+	outfile = sys.argv[1] 
+else:
+	outfile = None 
+
+Nruns = 10
+eps = np.logspace(-6, 0, Nruns)
+
+tol = 1e-12
+
+def getIt(eps, opt, gauss):
+
+	N = 100
+
+	xb = 1 
+
+	x0 = np.linspace(0, xb, N+1)
+	Sigmat = lambda x: 1 
+	Sigmaa = lambda x: .1 
+
+	n = 8 
+
+	it = np.zeros(len(eps))
+
+	diff = np.zeros(len(eps))
+
+	for i in range(len(eps)):
+
+		sol = LD.Eddington(x0, n, lambda x: eps[i], lambda x: 1/eps[i], 
+			np.ones((n,N))*eps[i], OPT=opt, GAUSS=gauss)
+
+		x, phi, it[i] = sol.sourceIteration(tol, maxIter=200)
+
+		phi_ex = exactDiff(eps[i], 1/eps[i], eps[i], xb)
+
+		diff[i] = np.linalg.norm(phi - phi_ex(x), 2)/np.linalg.norm(phi_ex(x), 2)
+
+	return diff, it 
+
+diff00, it00 = getIt(eps, 0, 0)
+diff10, it10 = getIt(eps, 1, 0)
+diff01, it01 = getIt(eps, 0, 1)
+diff11, it11 = getIt(eps, 1, 1)
+diff20, it20 = getIt(eps, 2, 0)
+diff21, it21 = getIt(eps, 2, 1)
+
+plt.subplot(2,1,1)
+plt.loglog(eps, diff00, '-o', label='MHFEM Edges, No Gauss')
+plt.loglog(eps, diff10, '-o', label='Maintain Slopes, No Gauss')
+plt.loglog(eps, diff01, '-o', label='MHFEM Edges, Gauss')
+plt.loglog(eps, diff11, '-o', label='Maintain Slopes, Gauss')
+plt.loglog(eps, diff20, '-o', label='vanLeer, No Gauss')
+plt.loglog(eps, diff21, '-o', label='vanLeer, Gauss')
+
+plt.xlabel(r'$\epsilon$', fontsize=18)
+plt.ylabel('|Sn - Diffusion|', fontsize=18)
+plt.legend(loc='best')
+
+plt.subplot(2,1,2)
+plt.loglog(eps, it00, '-o', label='MHFEM Edges, No Gauss')
+plt.loglog(eps, it10, '-o', label='Maintain Slopes, No Gauss')
+plt.loglog(eps, it01, '-o', label='MHFEM Edges, Gauss')
+plt.loglog(eps, it11, '-o', label='Maintain Slopes, Gauss')
+plt.loglog(eps, it20, '-o', label='vanLeer, No Gauss')
+plt.loglog(eps, it21, '-o', label='vanLeer, Gauss')
+
+plt.xlabel(r'$\epsilon$', fontsize=18)
+plt.ylabel('Number of Iterations', fontsize=18)
+plt.legend(loc='best')
+if (outfile != None):
+	plt.savefig(outfile)
+else:
+	plt.show()

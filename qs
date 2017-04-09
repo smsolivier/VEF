b1041020f5311f3262eb4de49f54232561bf3a91
Sam Olivier <smsolivier@gmail.com>
2017-04-09 10:56:47 -0400

    paper work

---
+++ qs
@@ -0,0 +1,3 @@
+What to do about slopes on boundaries 
+How much detail to go into for methods 
+	need to show MHFEM/LLDG discretization
--- tex/paper/perm_dl.py
+++ tex/paper/perm_dl.py
@@ -11,10 +11,12 @@ import ld as LD
 
 from exactDiff import exactDiff
 
+from hidespines import * 
+
 ''' compare the permuations of linear representation in diffusion limit ''' 
 
 if (len(sys.argv) > 1):
-	outfile = sys.argv[1] 
+	outfile = sys.argv[1:] 
 else:
 	outfile = None 
 
@@ -52,37 +54,41 @@ def getIt(eps, opt, gauss):
 
 	return diff, it 
 
-diff00, it00 = getIt(eps, 0, 0)
-diff10, it10 = getIt(eps, 1, 0)
+# diff00, it00 = getIt(eps, 0, 0)
+# diff10, it10 = getIt(eps, 1, 0)
 diff01, it01 = getIt(eps, 0, 1)
 diff11, it11 = getIt(eps, 1, 1)
-diff20, it20 = getIt(eps, 2, 0)
+# diff20, it20 = getIt(eps, 2, 0)
 diff21, it21 = getIt(eps, 2, 1)
 
-plt.subplot(2,1,1)
-plt.loglog(eps, diff00, '-o', label='MHFEM Edges, No Gauss')
-plt.loglog(eps, diff10, '-o', label='Maintain Slopes, No Gauss')
-plt.loglog(eps, diff01, '-o', label='MHFEM Edges, Gauss')
-plt.loglog(eps, diff11, '-o', label='Maintain Slopes, Gauss')
-plt.loglog(eps, diff20, '-o', label='vanLeer, No Gauss')
-plt.loglog(eps, diff21, '-o', label='vanLeer, Gauss')
-
-plt.xlabel(r'$\epsilon$', fontsize=18)
-plt.ylabel('|Sn - Diffusion|', fontsize=18)
-plt.legend(loc='best')
-
-plt.subplot(2,1,2)
-plt.loglog(eps, it00, '-o', label='MHFEM Edges, No Gauss')
-plt.loglog(eps, it10, '-o', label='Maintain Slopes, No Gauss')
-plt.loglog(eps, it01, '-o', label='MHFEM Edges, Gauss')
-plt.loglog(eps, it11, '-o', label='Maintain Slopes, Gauss')
-plt.loglog(eps, it20, '-o', label='vanLeer, No Gauss')
-plt.loglog(eps, it21, '-o', label='vanLeer, Gauss')
+plt.figure()
+# plt.loglog(eps, it00, '-o', label='MHFEM Edges, No Gauss')
+# plt.loglog(eps, it10, '-o', label='Maintain Slopes, No Gauss')
+plt.loglog(eps, it01, '-o', clip_on=False, label='None')
+plt.loglog(eps, it11, '-o', clip_on=False, label='MHFEM Edges')
+# plt.loglog(eps, it20, '-o', label='vanLeer, No Gauss')
+plt.loglog(eps, it21, '-o', clip_on=False, label='van Leer on Centers')
 
 plt.xlabel(r'$\epsilon$', fontsize=18)
 plt.ylabel('Number of Iterations', fontsize=18)
-plt.legend(loc='best')
+plt.legend(loc='best', frameon=False)
+hidespines(plt.gca())
 if (outfile != None):
-	plt.savefig(outfile)
+	plt.savefig(outfile[0])
+
+plt.figure()
+# plt.loglog(eps, diff00, '-o', label='MHFEM Edges, No Gauss')
+# plt.loglog(eps, diff10, '-o', label='Maintain Slopes, No Gauss')
+plt.loglog(eps, diff01, '-o', clip_on=False, label='None')
+plt.loglog(eps, diff11, '-o', clip_on=False, label='MHFEM Edges')
+# plt.loglog(eps, diff20, '-o', label='vanLeer, No Gauss')
+plt.loglog(eps, diff21, '-o', clip_on=False, label='van Leer on Centers')
+
+plt.xlabel(r'$\epsilon$', fontsize=18)
+plt.ylabel('|Sn - Diffusion|', fontsize=18)
+plt.legend(loc='best', frameon=False)
+hidespines(plt.gca())
+if (outfile != None):
+	plt.savefig(outfile[1])
 else:
 	plt.show()

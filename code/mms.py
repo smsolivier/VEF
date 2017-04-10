9e084c559b1d543eb5187b10d0405ab031e76d2a
smsolivier <smsolivier@gmail.com>
2017-04-10 13:34:38 -0500

    q as function fix

--- code/mms.py
+++ code/mms.py
@@ -66,6 +66,7 @@ n = 8
 
 Sigmaa = lambda x: .1
 Sigmat = lambda x: 1
+q = lambda x, mu: 1 
 
 xb = 5
 
@@ -73,25 +74,25 @@ tol = 1e-10
 
 # make solver objects 
 ed = [LD.LD(np.linspace(0, xb, x+1), n, Sigmaa, 
-	Sigmat, np.ones((n,x))) for x in N]
+	Sigmat,q) for x in N]
 
 ed00 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
-	Sigmat, np.ones((n, x)), OPT=0, GAUSS=0) for x in N]
+	Sigmat, q, OPT=0, GAUSS=0) for x in N]
 
 ed01 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
-	Sigmat, np.ones((n, x)), OPT=0, GAUSS=1) for x in N]
+	Sigmat, q, OPT=0, GAUSS=1) for x in N]
 
 ed10 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
-	Sigmat, np.ones((n, x)), OPT=1, GAUSS=0) for x in N]
+	Sigmat, q, OPT=1, GAUSS=0) for x in N]
 
 ed11 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
-	Sigmat, np.ones((n, x)), OPT=1, GAUSS=1) for x in N]
+	Sigmat, q, OPT=1, GAUSS=1) for x in N]
 
 ed20 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
-	Sigmat, np.ones((n,x)), OPT=2, GAUSS=0) for x in N]
+	Sigmat,q, OPT=2, GAUSS=0) for x in N]
 
 ed21 = [LD.Eddington(np.linspace(0, xb, x+1), n, Sigmaa, 
-	Sigmat, np.ones((n, x)), OPT=2, GAUSS=1) for x in N]
+	Sigmat, q, OPT=2, GAUSS=1) for x in N]
 
 # get order of accuracy 
 # err = getOrder(ed, N, tol, 'LD')

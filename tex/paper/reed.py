ed7337ad2afe1fbf16ab5f0388c652785fea091d
Sam Olivier <smsolivier@gmail.com>
2017-04-24 09:49:32 -0500

    paper work

--- tex/paper/reed.py
+++ tex/paper/reed.py
@@ -8,33 +8,27 @@ sys.path.append('../../code')
 
 import ld as LD 
 
-Sigmat = lambda x: 100*(x<2) + .001*(x>=2)*(x<4) + \
+Sigmat = lambda x: 50*(x<2) + .001*(x>=2)*(x<4) + \
 	1*(x>=4)*(x<6) + 5*(x>=6)*(x<7) + 1*(x>=7)*(x<=8)
-Sigmaa = lambda x: 100*(x<2) + .1*(x>=4)*(x<6) + 5*(x>=6)*(x<7) + .1*(x>=7)*(x<=8) 
-q = lambda x, mu: 100*(x<2) + 1*(x>=7)*(x<=8)
+Sigmaa = lambda x: 50*(x<2) + .1*(x>=4)*(x<6) + 5*(x>=6)*(x<7) + .1*(x>=7)*(x<=8) 
+q = lambda x, mu: 50*(x<2) + 1*(x>=7)*(x<=8)
 
 xb = 8
-
-x = np.linspace(0, xb, 1000)
-plt.figure()
-plt.plot(x, Sigmat(x), '--', label=r'$\Sigma_t(x)$')
-plt.figure()
-plt.plot(x, Sigmaa(x), label=r'$\Sigma_a(x)$')
-plt.figure()
-plt.plot(x, q(x, 0), label=r'$Q(x)$')
-# plt.legend(loc='best')
-plt.show()
 
 n = 8 
 tol = 1e-10 
 
-N = 250
+N = 750
 x = np.linspace(0, xb, N+1)
 
+ld = LD.LD(x, n, Sigmaa, Sigmat, q)
 ed = LD.Eddington(x, n, Sigmaa, Sigmat, q)
 
-x, phi, it = ed.sourceIteration(tol)
+x, phi, it = ld.sourceIteration(tol)
+xe, phie, ite = ed.sourceIteration(tol)
 
-plt.plot(x, phi)
+plt.plot(x, phi, label='SI')
+plt.plot(xe, phie, label='VEF')
+plt.legend(loc='best')
 plt.show()
 
